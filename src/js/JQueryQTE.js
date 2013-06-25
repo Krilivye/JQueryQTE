7f7e51e4de5b67c761de8a196f365f0cf67c4c69
krilivye <pierre.bousquie@gmail.com>
2013-06-25 19:59:15 +0200

    Allow custom success/fail methods

--- src/js/JQueryQTE.js
+++ src/js/JQueryQTE.js
@@ -1,81 +1,99 @@
+//
+// create closure
+//
+(function($) {
+
+
     //
-    // create closure
+    // plugin definition
     //
-    (function($) {
-      //
-      // plugin definition
-      //
-      $.fn.qte = function(options) {
-        debug(this);
+    $.fn.qte = function(options) {
         // build main options before element iteration
         var opts = $.extend({}, $.fn.qte.defaults, options);
         // iterate and reformat each matched element
         return this.each(function() {
-          $this = $(this);
-          // build element specific options
-          var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
-                
-          var attempt = 0    
-          $.fn.qte.pop($this,o,0);
-          
+            $this = $(this);
+            // build element specific options
+            var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
+            
+            $.fn.qte.pop($this,o);
+            
         });
-      };
-      //
-      // private function for debugging
-      //
-      function debug($obj) {
+    };
+
+    //
+    // plugin defaults
+    //
+    $.fn.qte.defaults = {
+        key:'SPACE',
+        time:0,
+        delay:0,
+        attempt:0
+    };
+    //
+    // private function for debugging
+    //
+    function debug($obj) {
         if (window.console && window.console.log)
-          window.console.log('QTE Call' + $obj.size());
-      };
-      //
-      // define and expose our format function
-      //
-      $.fn.qte.pop = function(obj,o,attempt) {
+            window.console.log('QTE Call' + $obj.size());
+    };
+    //
+    // define and expose our format function
+    //
+    $.fn.qte.pop = function(obj,options) {
         var qte = $("<div class='QTE'/>");
-        if (o.time === 0){
-            qte.html(o.key)
-        }else{
-            setTimeout(function(){
-                if(o.delay != 0){
-                    setTimeout(function(){
-                        qte.empty();
-                    },o.delay)
-                }
-                qte.html(o.key)
-            },o.time);
-        }
+	var attempt = 0;
+	$.fn.qte.display(qte,options,attempt);
+
         obj.keydown(function(event) {
-            attempt = attempt + 1
-                if ( String.fromCharCode(event.which) === (o.key).toUpperCase() ) {
-                qte.html('SUCCES')
-                //qte.html(o.key)
+            attempt = attempt + 1;
+            if ( String.fromCharCode(event.which) === (options.key).toUpperCase() ) {
+                $.fn.qte.succes(qte);
             }
             else{
                 
-                qte.html('Fail!')
-                if(attempt < o.attempt){
-                    qte.html('Fail! Try again')
-                }
-                if(o.attempt === attempt){
+                $.fn.qte.fail(qte,options,attempt);
+
+                if(options.attempt === attempt){
                     obj.unbind('keydown')
                 }
             }
             
         })
         obj.append(qte)
-      };
-      //
-      // plugin defaults
-      //
-      $.fn.qte.defaults = {
-        key:'SPACE',
-        time:0,
-        delay:0,
-        attempt:0
-      };
+    };
+
+
+    $.fn.qte.succes = function(obj){
+	obj.html('SUCCES')
+    }
+
+    $.fn.qte.fail = function(obj,options,attempt){
+	
+        if(attempt < options.attempt){
+                    obj.html('Fail! Try again')
+        }else{
+	    obj.html('Fail!')
+	}
+    }
+
+    $.fn.qte.display = function(obj,options,attempt){
+       if (options.time === 0){
+            obj.html(options.key)
+        }else{
+            setTimeout(function(){
+                if(options.delay != 0){
+                    setTimeout(function(){
+                        obj.empty();
+                    },options.delay)
+                }
+                obj.html(options.key)
+            },options.time);
+        }
+    }
     //
     // end of closure
     //
-    })(jQuery);
+})(jQuery);
 
 
